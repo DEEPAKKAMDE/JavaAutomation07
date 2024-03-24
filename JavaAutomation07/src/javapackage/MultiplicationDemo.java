306475586417e9af462b8c4794b5642210909f50
DEEPAKKAMDE <deepakkamde03@gmail.com>
2024-03-24 13:22:57 +0530

    Second Commit!!

---
+++ JavaAutomation07/src/javapackage/MultiplicationDemo.java
@@ -0,0 +1,16 @@
+package javapackage;
+
+public class MultiplicationDemo {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=5;
+		int mult=a*b;
+		System.out.println(mult);
+		
+		
+
+	}
+
+}
